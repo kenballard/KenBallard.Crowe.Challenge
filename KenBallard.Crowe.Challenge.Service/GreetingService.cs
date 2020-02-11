82b093f3b736d0beca8b1e74d68fb7a53e624fb3
Ken Ballard <mail@kenballard.me>
2020-02-11 12:27:41 -0600

    initial commit

---
+++ KenBallard.Crowe.Challenge.Service/GreetingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KenBallard.Crowe.Challenge.Service
+{
+
+    public class GreetingService : IGreetingService
+    {
+         public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ KenBallard.Crowe.Challenge.Service/IGreetingService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KenBallard.Crowe.Challenge.Service
+{
+    /// <summary>
+    /// Service to greet stakeholders.
+    /// </summary>
+    public interface IGreetingService
+    {
+        /// <summary>
+        /// Gets the acceptable greeting.
+        /// </summary>
+        /// <returns>The acceptable greeting.</returns>
+        string Get();
+    }
+}
